78c8db266148ee5015c588783387cf17e2b2c09e
Batanov Vasiliy <wisakejjak@gmail.com>
2024-04-30 20:00:04 +0300

    Reworked get_sources script

--- get_sources.sh
+++ get_sources.sh
@@ -6,9 +6,17 @@ fi
 target_dir="$1"
 mkdir -vp "$target_dir/sources"
 chmod -v a+wt "$target_dir/sources"
-cd "$target_dir/sources" || exit
-if [ -f "wget_sources" ]; then
-    wget --input-file=wget_sources --continue
+cd "$target_dir/sources" || exit 1
+if [ ! -f "wget_sources" ]; then
+	echo "File wget_sources not found... Downloading"
+	curl https://raw.githubusercontent.com/wisakejjak/LFScripts/main/wget_sources -o wget_sources
+	if [ ! -f "wget_sources" ]; then
+		echo "File wget_sources downloaded successfully."
+
+	else
+		echo "File wget_sources not downloaded... Exiting"
+		exit 1
 fi
-chown root:root $target_dir/sources/*
+wget --input-file=wget_sources --continue
+chown root:root "$PWD/"*
 echo "Job done."
